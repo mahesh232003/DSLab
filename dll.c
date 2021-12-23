fe4f5281050cbaacc24f41d7f7c723723def9f57
mahesh232003 <79948899+mahesh232003@users.noreply.github.com>
2021-12-23 16:43:33 +0530

    Update dll.c

--- dll.c
+++ dll.c
@@ -83,7 +83,7 @@ void search(node *first,int ele)
     {
         while(temp!=NULL)
         {
-            if (temp->data=ele)
+            if (temp->data==ele)
             {
                 flag=1;
                 break;
@@ -103,9 +103,151 @@ void search(node *first,int ele)
         }
     }
 }
+node* sort(node *first)
+{
+    int x;
+    node *temp=first,*temp1;
+    if(first==NULL)
+    {
+        printf("No elements to sort\n");
+    }
+    else
+    {
+        for(temp=first;temp->right!=NULL;temp=temp->right)
+        {
+            for(temp1=temp->right;temp1!=NULL;temp1=temp1->right)
+            {
+                if(temp->data>temp1->data)
+                {
+                    x=temp1->data;
+                    temp1->data=temp->data;
+                    temp->data=x;
+                }
+            }
+        }
+    }
+    return first;
+}
+node *delete(node *first,int x)
+{
+    node *temp=first,*temp1;
+    int flag=0;
+    if (first==NULL)
+    {
+        printf("NO elements to delete\n");
+    }
+    else
+    {
+        if (first->data==x)
+        {
+            first=first->right;
+            free(temp);
+        }
+        else
+        {
+            temp1=first->right;
+            while(temp!=NULL)
+            {
+                if (temp->data==x)
+                {
+                    flag=1;
+                    break;
+                }
+                else
+                {
+                    temp1=temp;
+                    temp=temp->right;
+                }
+            }
+            if(flag)
+            {
+                temp1->right=temp->right;
+                free(temp);
+            }
+            else
+            {
+                printf("Element not found to delete\n");
+            }
+        }
+    }
+    return first;
+}
+node* insertatbegin(node *first,int x)
+{
+    node *new,*temp;
+    new=(node*)malloc(sizeof(node));
+    new->data=x;
+    new->right=NULL;
+    new->left=NULL;
+    if (first==NULL)
+    {
+        first=new;
+    }
+    else
+    {
+        new->right=first;
+        first->left=new;
+        first=new;
+    }
+    return first;
+}
+node* insertatend(node *first,int x)
+{
+    node *new,*temp=first;
+    new=(node*)malloc(sizeof(node));
+    new->data=x;
+    new->right=NULL;
+    new->left=NULL;
+    if(first==NULL)
+    {
+        first=new;
+    }
+    else
+    {
+        while(temp->right!=NULL)
+        {
+            temp=temp->right;
+        }
+        temp->right=new;
+        new->left=temp;
+    }
+    return first;
+}
+node* insertatpos(node *first,int x,int pos)
+{
+    int i;
+    node *temp=first;
+    int n=count(first);
+    if (pos>1&&pos<n)
+    {
+        node *new=(node*)malloc(sizeof(node));
+        new->data=x;
+        new->right=NULL;
+        new->left=NULL;
+        for (i=1;i<pos-1;i++)
+        {
+            temp=temp->right;
+        }
+        new->right=temp->right;
+        temp->right=new;
+    }
+    return first;
+}
+node* reverse(node *first)
+{
+    node *pre=first,*prev=NULL,*save;
+    while(pre!=NULL)
+    {
+        save=pre->next;
+        pre->next=prev;
+        prev=pre;
+        pre=save;
+    }
+    return prev;
+}
 int main()
 {
-    int c,ele;
+    int c,ele,x,x1,x2,x3,pos;
     node *head=NULL;
     head=create(head);
     display(head);
@@ -114,6 +256,29 @@ int main()
     printf("Enter element to search\n");
     scanf("%d",&ele);
     search(head,ele);
+    display(head);
+    printf("After sorting\n");
+    head=sort(head);
+    display(head);
+    printf("Enter element to delete\n");
+    scanf("%d",&x);
+    head=delete(head,x);
+    display(head);
+    printf("Enter element to insert at beginning\n");
+    scanf("%d",&x1);
+    head=insertatbegin(head,x1);
+    display(head);
+    printf("Enter element to insert at ending\n");
+    scanf("%d",&x2);
+    head=insertatend(head,x2);
+    display(head);
+    printf("Enter element to insert at a postion\n");
+    scanf("%d",&x3);
+    printf("Enter position to insert\n");
+    scanf("%d",&pos);
+    head=insertatpos(head,x3,pos);
+    display(head);
     return 0;
 }
+
 
