778494cd6e4448dd15720dec4c68fc3555165c21
mahesh232003 <79948899+mahesh232003@users.noreply.github.com>
2022-01-05 19:24:20 +0530

    Update arrayoperations on linked list.c

--- arrayoperations on linked list.c
+++ arrayoperations on linked list.c
@@ -1,177 +1,299 @@
-#include <stdio.h>
-int a[10];
-int n;
-void read()
+#include<stdio.h>
+#include<stdlib.h>
+struct list1
 {
-    int i;
-    printf("Enter array elementes\n");
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-}
-void display()
+        int data;
+        struct list1 *next;
+};
+typedef struct list1 node;
+node* addnode(node* first,node *new)
 {
-    int i;
-    for(i=0;i<n;i++)
-    {
-        printf("%3d",a[i]);
-    }
-}
-void insert(int pos,int ele)
-{
-    int i;
-    for(i=n-1;i>=pos-1;i--)
-    {
-        a[i+1]=a[i];
-    }
-    n++;
-    a[pos-1]=ele;
-}
-void sort()
-{
-    int i,j,t;
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n-i-1;j++)
+        node *temp=first;
+        if (first==NULL)
         {
-            if(a[j]>a[j+1])
-            {
-                t=a[j];
-                a[j]=a[j+1];
-                a[j+1]=t;
-            }
+                first=new;
         }
-    }
-}
-void search(int se)
-{
-    int i,flag=0,position;
-    for(i=0;i<n;i++)
-    {
-        if (a[i]==se)
+        else
         {
-            flag=1;
-            position=i;
-            break;
-        }
-    }
-    if (flag==1)
-    {
-        printf("Element found at position %d\n",position);
-    }
-    else
-    {
-        printf("Element not found\n");
-    }
-}
-void delete(int del)
-{
-    int i,j;
-    for (i=0;i<n;i++)
-    {
-        if (a[i]==del)
-        {
-            for (j=i;j<n-1;j++)
-            {
-                a[j]=a[j+1];
-            }
-            n--;
-            break;
-        }
-    }
-}
-void reverse()
-{
-    int i,j,t;
-    for(i=0,j=n-1;i<j;i++,j--)
-    {
-        t=a[i];
-        a[i]=a[j];
-        a[j]=t;
-    }
-}
-void RemoveDuplicates()
-{
-    int i,j,k;
-    for(i=0;i<n;i++)
-    {
-        for(j=i+1;j<n;j++)
-        {
-            if (a[i]==a[j])
-            {
-                for(k=j;k<n-1;k++)
+                while(temp->next!=NULL)
                 {
-                    a[k]=a[k+1];
+                        temp=temp->next;
                 }
-                n--;
-                j--;
-            }
+                temp->next=new;
         }
-    }
+        return first;
 }
-int main()
+void sort(node *first)
 {
-    int i,pos,ele,ch,del,se;
-    printf("Enter your choice\n");
-    printf("1:read\t2:display\t3:insert\t4:sort\t5:search\t6:delete\t7:reverse\t8:RemoveDuplicates\t9:exit\n");
-    scanf("%d",&ch);
-    printf("Enter the size\n");
-    scanf("%d",&n);
-    while(ch!=9)
-    {
-        switch(ch)
+        node *temp1,*temp2,*new;
+        new=(node*)malloc(sizeof(node));
+        new->data=0;
+        new->next=NULL;
+        if(first==NULL)
         {
-            case 1:read();
-                   break;
-            case 2:display();
-                   break;
-            case 3:printf("Enter position to insert\n");
-                   scanf("%d",&pos);
-                   printf("Enter element to insert\n");
-                   scanf("%d",&ele);
-                   insert(pos,ele);
-                   display();
-                   break;
-            case 4:printf("\nBefore sorting\n");
-                   display();
-                   sort();
-                   printf("\nAfter sorting\n");
-                   display();
-                   break;
-            case 5:printf("Enter element to search\n");
-                   scanf("%d",&se);
-                   search(se);
-                   break;
-            case 6:printf("Enter element to delete\n");
-                   scanf("%d",&del);
-                   printf("\nBefore deletion\n");
-                   display();
-                   delete(del);
-                   printf("\n");
-                   printf("\nAfter deletion\n");
-                   display();
-                   break;
-            case 7:printf("\nBefore reversing\n");
-                   display();
-                   reverse();
-                   printf("\n");
-                   printf("\nAfter reversing\n");
-                   display();
-                   break;
-            case 8:printf("\nBefore removing duplicates\n");
-                   display();
-                   RemoveDuplicates();
-                   printf("\n");
-                   printf("\nAfter removing duplicates\n");
-                   display();
-                   break;
-            default:printf("Enter a valid input");
-                   break;
+                printf("there is no list to sort");
         }
-        printf("\n");
-        printf("1:read\t2:display\t3:insert\t4:sort\t5:search\t6:delete\t7:reverse\t8:RemoveDuplicates\t9:exit\n");
-        scanf("%d",&ch);
-    }
-    return 0;
+        else
+        {
+                for(temp1=first;temp1->next!=NULL;temp1=temp1->next)
+                {
+                        for(temp2=temp1->next;temp2!=NULL;temp2=temp2->next)
+                        {
+                                if(temp1->data > temp2->data)
+				{
+                                        new->data = temp1->data;
+                                        temp1->data = temp2->data;
+                                        temp2->data=new->data;
+                                }
+                        }
+                }
+        }
+}
+node* create(node *first)
+{
+        node *new,*prev;
+        int x;
+        printf("Enter the data value(enter -1 to stop)\n");
+        scanf("%d",&x);
+        while(x!=-1)
+        {
+                new=(node *)malloc(sizeof(node));
+                new->data=x;
+                new->next=NULL;
+                if(first==NULL)
+                {
+                        first=new;
+                }
+                else
+                {
+                        prev->next=new;
+                }
+                prev=new;
+                printf("enter data (enter -1 to stop)\t");
+                scanf("%d",&x);
+        }
+        return first;
+}
+void display(node *first)
+{
+        node *temp=first;
+        if(first==NULL)
+        {
+                printf("No list to print");
+        }
+        else
+        {
+                while(temp!=NULL)
+                {
+                        printf("|%d|->",temp->data);
+                        temp=temp->next;
+                }
+                printf("NULL\n");
+        }
+}
+node * concatenate(node* l1,node *l2)
+{
+        node *l3=NULL;
+        node *temp;
+        if(l1==NULL)
+                l3=l2;
+        else if (l2==NULL)
+                l3=l1;
+        else
+        {
+                node *new;
+                temp=l1;
+                while(temp!=NULL)
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=temp->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        temp=temp->next;
+                }
+                temp=l3;
+                while(temp->next!=NULL)
+                {
+                        temp=temp->next;
+                }
+                temp->next=l2;
+                return l3;
+        }
+}
+node * listunion(node *l1,node *l2)
+{
+        node *l3=NULL,*new;
+        while(l1!=NULL && l2!=NULL)
+        {
+                if(l1->data<l2->data)
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l1->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l1=l1->next;
+                }
+                else if(l2->data<l1->data)
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l2->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l2=l2->next;
+                }
+                else
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l2->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l1=l1->next;
+                        l2=l2->next;
+                }
+        }
+        if(l1==NULL)
+        {
+                while(l2!=NULL)
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l2->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l2=l2->next;
+                }
+        }
+        if(l2==NULL)
+        {
+                while(l1!=NULL)
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l1->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l1=l1->next;
+                }
+        }
+        return l3;
+}
+node * listmerge(node *l1,node *l2)
+{
+        node *l3=NULL,*new,*new1,*new2;
+        while(l1!=NULL && l2!=NULL)
+        {
+                if(l1->data<l2->data)
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l1->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l1=l1->next;
+                }
+                else if(l2->data<l1->data)
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l2->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l2=l2->next;
+                }
+                else
+                {
+                        new1=(node*)malloc(sizeof(node));
+			new2=(node*)malloc(sizeof(node));
+                        new1->data=new2->data=l1->data;
+                        new1->next=new2->next=NULL;
+                        l3=addnode(l3,new1);
+                        l3=addnode(l3,new2);
+                        l1=l1->next;
+                        l2=l2->next;
+                }
+        }
+        if(l1==NULL)
+        {
+                while(l2!=NULL)
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l2->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l2=l2->next;
+                }
+        }
+        if(l2==NULL)
+        {
+                while(l1!=NULL)
+                {
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l1->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l1=l1->next;
+                }
+        }
+        return l3;
+}
+node *listintersection(node*l1,node*l2)
+{
+        node *l3=NULL;
+        node *new;
+        while(l1!=NULL && l2!=NULL)
+        {
+                if(l1->data <l2->data)
+                {
+                        l1=l1->next;
+                }
+                else if (l1->data>l2->data)
+                {
+                        l2=l2->next;
+                }
+                else
+                {
+                        new=(node *)malloc(sizeof(node));
+                        new->data=l1->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l1=l1->next;
+                        l2=l2->next;
+                }
+        }
+        return l3;
+}
+void main()
+{
+        int ch;
+        node *l1=NULL,*l2=NULL,*l3;
+        l1=create(l1);
+        display(l1);
+        l2=create(l2);
+        display(l2);
+        printf("after sortig the lists");
+        sort(l1);
+        sort(l2);
+        display(l1);
+        display(l2);
+        while(1)
+        {
+                printf("enter your choice\n");
+                printf("1:concatenate\n2:union\n3:intersection\n4:merge\n5:exit\n");
+                scanf("%d",&ch);
+                switch(ch)
+                {
+                        case 1: l3=concatenate(l1,l2);
+                                display(l3);
+                                break;
+                        case 2: l3=listunion(l1,l2);
+                                display(l3);
+                                break;
+                        case 3: l3=listintersection(l1,l2);
+                                display(l3);
+                                break;
+                        case 4: l3=listmerge(l1,l2);
+                                display(l3);
+                                break;
+                        case 5: exit(0);
+                        default:printf("enter the correct choice ");
+                }
+        }
+
+
 }
